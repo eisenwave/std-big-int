7d740ab3422956f745f90cb472f9d34029f9ae89
Matt Borland <matt@mattborland.com>
2026-04-09 15:02:08 -0400

    Fix generation script issues (#2)

--- include/beman/std-big-int/std-big-int.hpp
+++ include/beman/std-big-int/std-big-int.hpp
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef BEMAN_STD - BIG - INT_STD - BIG - INT_HPP
-    #define BEMAN_STD -BIG - INT_STD - BIG - INT_HPP
+#ifndef BEMAN_STD_BIG_INT_STD_BIG_INT_HPP
+#define BEMAN_STD_BIG_INT_STD_BIG_INT_HPP
 
-    #include <beman/std-big-int/todo.hpp>
+#include <beman/std-big-int/todo.hpp>
 
-#endif // BEMAN_STD-BIG-INT_STD-BIG-INT_HPP
+#endif // BEMAN_STD_BIG_INT_STD_BIG_INT_HPP
