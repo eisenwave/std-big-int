a1c048cab65a5f7532a5fd4fbd64a091c692e69f
Matt Borland <matt@mattborland.com>
2026-04-09 14:48:55 -0400

    Fix build script generation issues

--- include/beman/std-big-int/todo.hpp
+++ include/beman/std-big-int/todo.hpp
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef BEMAN_STD - BIG - INT_TODO_HPP
-    #define BEMAN_STD -BIG - INT_TODO_HPP
+#ifndef BEMAN_STD_BIG_INT_TODO_HPP
+#define BEMAN_STD_BIG_INT_TODO_HPP
 
-namespace beman::std - big - int {
+namespace beman::std_big_int {} // namespace beman::std_big_int
 
-    // TODO
-
-} // namespace beman::std
-
-#endif // BEMAN_STD-BIG-INT_TODO_HPP
+#endif // BEMAN_STD_BIG_INT_TODO_HPP
