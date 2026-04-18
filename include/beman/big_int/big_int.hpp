a7de884d58a15b855fa1cff1420c86784292b47c
Eisenwave <me@eisenwave.net>
2026-04-18 10:00:17 +0200

    finish from_chars and literal operator

--- include/beman/big_int/big_int.hpp
+++ include/beman/big_int/big_int.hpp
@@ -2381,6 +2381,8 @@ namespace detail {
                                   : -1;
 }
 
+} // namespace detail
+
 template <size_t b, class A>
 constexpr std::from_chars_result
 from_chars(const char* const begin, const char* const end, basic_big_int<b, A>& out, const int base) {
@@ -2402,7 +2404,7 @@ from_chars(const char* const begin, const char* const end, basic_big_int<b, A>& 
                                         : static_cast<unsigned char>(0);
     bool       at_least_one_digit = false;
     for (; p != end; ++p) {
-        const int digit = digit_value(*p);
+        const int digit = detail::digit_value(*p);
         if (digit < 0 || digit >= base) {
             break;
         }
@@ -2438,16 +2440,19 @@ from_chars(const char* const begin, const char* const end, basic_big_int<b, A>& 
     return {p, std::errc{}};
 }
 
+namespace detail {
+
 using std::from_chars;
 
-// Like `std::from_chars`, but detects the base automatically
-// based on the `0x`, `0b`, or `0` prefix.
+// Detects the base automatically based on the `0x`, `0b`, or `0` prefix
+// and dispatches to `from_chars` with the identified base.
+// Leading minus signs are not supported.
 template <class T>
 [[nodiscard]] constexpr std::from_chars_result
 from_chars_auto_base(const char* const begin, const char* const end, T& out)
     requires requires { from_chars(begin, end, out, 10); }
 {
-    if (begin == end) {
+    if (begin == end || *begin < '0' || *begin > '9') {
         return {end, std::errc::invalid_argument};
     }
     if (*begin != '0' || end - begin <= 1) {
@@ -2470,40 +2475,121 @@ from_chars_auto_base(const char* const begin, const char* const end, T& out)
     return from_chars(begin, end, out, 8);
 }
 
+// Helper class which assists in removing digit separators from literals.
+template <char... digits>
+struct literal_buffer {
+  private:
+    // The sizeof... is necessary here because of:
+    // https://github.com/llvm/llvm-project/issues/79750
+    static constexpr char        raw_buffer[sizeof...(digits)]{digits...};
+    static constexpr const char* raw_end = raw_buffer + sizeof(raw_buffer);
+
+    static consteval std::size_t make_buffer_impl(const char* begin, const char* const end, char* out = nullptr) {
+        bool        digit_separator_allowed = false;
+        std::size_t length                  = 0;
+        for (; begin != end; ++begin) {
+            if (*begin == '\'') {
+                if (!digit_separator_allowed) {
+                    return 0;
+                }
+                digit_separator_allowed = false;
+            } else {
+                digit_separator_allowed = true;
+                ++length;
+                if (out != nullptr) {
+                    *out++ = *begin;
+                }
+            }
+        }
+        return length;
+    }
+
+    static constexpr std::size_t result_length = make_buffer_impl(raw_buffer, raw_end);
+
+    [[nodiscard]] static consteval auto make_buffer() {
+        if constexpr (result_length == 0) {
+            return std::array<char, 0>{};
+        } else {
+            std::array<char, result_length> result;
+            make_buffer_impl(raw_buffer, raw_end, result.data());
+            return result;
+        }
+    }
+
+  public:
+    static constexpr std::array<char, result_length> value = make_buffer();
+};
+
 BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
 BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_GCC("-Wpadded")
-struct big_int_and_errc {
-    big_int   value_or_limb_count;
-    std::errc ec;
+struct parse_non_allocating_result {
+    big_int            value;
+    big_int::size_type limb_count;
+    std::errc          ec;
 };
 BEMAN_BIG_INT_DIAGNOSTIC_POP()
 
-// Returns the result of parsing a `big_int` using `from_chars_auto_base`.
-// However, if the result is too large to fit into inplace storage,
-// `{limb_count(), std::errc::result_out_of_range}` is returned.
-[[nodiscard]] constexpr big_int_and_errc parse_non_allocating(const char* const begin, const char* const end) {
-    big_int result;
-    const auto [p, ec] = from_chars_auto_base(begin, end, result);
-    if (ec != std::errc{}) {
-        return {0, ec};
+template <std::array buffer>
+struct parse_non_allocating {
+  private:
+    static_assert(std::is_same_v<typename decltype(buffer)::value_type, char>);
+
+    static constexpr const char* begin = buffer.data();
+    static constexpr const char* end   = begin + buffer.size();
+
+    // Returns the result of parsing a `big_int` using `from_chars_auto_base`.
+    // However, if the result is too large to fit into inplace storage,
+    // `{limb_count(), std::errc::result_out_of_range}` is returned.
+    [[nodiscard]] static consteval parse_non_allocating_result operator()() {
+        big_int result;
+        const auto [p, ec] = from_chars_auto_base(begin, end, result);
+        if (ec != std::errc{}) {
+            return {0, 0, ec};
+        }
+        if (p != end) {
+            return {0, 0, std::errc::invalid_argument};
+        }
+        if (result.capacity() != 0) {
+            return {
+                .value      = 0,
+                .limb_count = result.size(),
+                .ec         = std::errc::result_out_of_range,
+            };
+        }
+        return {
+            .value      = result,
+            .limb_count = result.size(),
+            .ec         = {},
+        };
     }
-    if (p != end) {
-        return {0, std::errc::invalid_argument};
-    }
-    if (result.capacity() != 0) {
-        return {result.size(), std::errc::result_out_of_range};
-    }
-    return {.value_or_limb_count = result, .ec = {}};
+
+    static constexpr auto result = operator()();
+
+  public:
+    static constexpr big_int   value      = result.value;
+    static constexpr auto      limb_count = result.limb_count;
+    static constexpr std::errc ec         = result.ec;
+};
+
+template <std::size_t limb_count>
+[[nodiscard]] consteval std::array<uint_multiprecision_t, limb_count>
+literal_operator_n_compute_limbs(const char* begin, const char* const end) {
+    std::array<uint_multiprecision_t, limb_count> result;
+    big_int                                       parsed;
+    const auto [p, ec] = detail::from_chars_auto_base(begin, end, parsed);
+    // We've already parsed this successfully once in parse_non_allocating,
+    // so there's no reason it would fail now.
+    BEMAN_BIG_INT_ASSERT(p == end);
+    BEMAN_BIG_INT_ASSERT(ec == std::errc{});
+    BEMAN_BIG_INT_ASSERT(parsed.size() == limb_count);
+    std::copy_n(parsed.representation().data(), limb_count, result.data());
+    return result;
 }
 
-// Helper variable template which prevents multiple constant evaluations of parse_non_allocating,
-// in case compilers don't memoize.
-// It also provides some convenience.
-template <char... digits>
-inline constexpr big_int_and_errc parse_non_allocating_v = []() constexpr {
-    static constexpr char buffer[]{digits...};
-    return parse_non_allocating(buffer, buffer + sizeof(buffer));
-}();
+template <std::array buffer>
+inline constexpr std::array<uint_multiprecision_t, detail::parse_non_allocating<buffer>::limb_count>
+    literal_operator_n_limbs_v = literal_operator_n_compute_limbs<detail::parse_non_allocating<buffer>::limb_count>(
+        buffer.data(), buffer.data() + buffer.size());
 
 } // namespace detail
 
@@ -2520,57 +2606,33 @@ BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_MSVC(4455)
 // clang-format off
 template <char... digits>
 [[nodiscard]] constexpr big_int operator""n()
-  //noexcept(detail::parse_non_allocating_v<digits...>.ec == std::errc{}) 
+  noexcept(detail::parse_non_allocating<detail::literal_buffer<digits...>::value>::ec == std::errc{}) 
   {
     // clang-format on
 
-    // For this user-defined literal, there are two radically distinct situations.
-    // We are either able to fit the parsed value into the inplace storage,
-    // in which case `operator""n()` simply copies the resulting `big_int`
-    // out of a `constexpr` variable;
-    // or the parsed value doesn't fit and needs to be dynamically allocated on the fly.
-    //
-    // This weirdness is caused only by the fact that we don't have non-transient allocations,
-    // meaning that we cannot create a `constexpr` variable that holds an allocation.
-    // This also prevents us from making `operator""n()` `consteval` rather than `constexpr`.
-    // Because it is only `constexpr`, it is important to handle the "small case" specially,
-    // so that no runtime parsing takes place.
-    if constexpr (detail::parse_non_allocating_v<digits...>.ec == std::errc{}) {
-        return detail::parse_non_allocating_v<digits...>.value_or_limb_count;
-    } else if constexpr (detail::parse_non_allocating_v<digits...>.ec == std::errc::invalid_argument) {
-        static_assert(false,
-                      "The given literal is not a valid integer-literal. "
-                      "This should not even be possible "
-                      "without explicitly providing template arguments to the user-defined literal.");
-    } else {
-        static_assert(detail::parse_non_allocating_v<digits...>.ec == std::errc::result_out_of_range);
-        static_assert(false);
+    // The first step is to do basic validation and to eliminate any digit separators.
+    constexpr const auto& buffer = detail::literal_buffer<digits...>::value;
+    static_assert(!buffer.empty(), "The given literal is not a valid integer-literal.");
 
+    if constexpr (detail::parse_non_allocating<buffer>::ec == std::errc{}) {
+        // The happy case is where the literal's integer value is small enough to fit into big_int without allocations.
+        // In that case, we've already parsed the literal, and simply return the pre-computed big_int.
+        return detail::parse_non_allocating<buffer>::value;
+    } else if constexpr (detail::parse_non_allocating<buffer>::ec == std::errc::result_out_of_range) {
+        // The unhappy case is where allocations are required.
         // While we don't have non-transient allocations and cannot store a constexpr big_int,
         // we can precompute a constexpr limb array which is used for fast runtime initialization.
-        static constexpr char buffer[]{digits...};
         // We already know the limb count from the previous parsing attempt:
-        static constexpr auto dynamic_limb_count =
-            static_cast<std::size_t>(detail::parse_non_allocating_v<digits...>.value_or_limb_count);
-        // Unfortunately, we need to parse a second time to also obtain the limb data.
-        static constexpr auto limbs = [] {
-            big_int parsed;
-            const auto [p, ec] = detail::from_chars_auto_base(std::begin(buffer), std::end(buffer), parsed);
-            // We've already parsed this successfully once in parse_non_allocating,
-            // so there's no reason it would fail now.
-            BEMAN_BIG_INT_ASSERT(p == std::end(buffer));
-            BEMAN_BIG_INT_ASSERT(ec == std::errc{});
-            BEMAN_BIG_INT_ASSERT(parsed.size() == dynamic_limb_count);
-            std::array<uint_multiprecision_t, dynamic_limb_count> result;
-            std::copy_n(parsed.representation().data(), dynamic_limb_count, result.data());
-            return result;
-        }();
+        constexpr const auto& limbs = detail::literal_operator_n_limbs_v<buffer>;
 
-        big_int result(std::begin(limbs), std::end(limbs));
+        big_int result(limbs.data(), limbs.data() + limbs.size());
         if (buffer[0] == '-') {
             detail::access_bypass::negate(result);
         }
         return result;
+    } else {
+        static_assert(detail::parse_non_allocating<buffer>::ec == std::errc::invalid_argument);
+        static_assert(false, "The given literal is not a valid integer-literal.");
     }
 }
 
@@ -2578,8 +2640,7 @@ template <char... digits>
 // https://github.com/llvm/llvm-project/issues/76394
 // clang-format off
 template <char... digits>
-[[nodiscard]] constexpr big_int operator""_n()
-// noexcept(noexcept(operator""n<digits...>())) 
+[[nodiscard]] constexpr big_int operator""_n() noexcept(noexcept(operator""n<digits...>())) 
 {
     return operator""n<digits...>();
 }
