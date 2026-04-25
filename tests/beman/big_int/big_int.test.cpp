7f5438f6fb2fb641c3e50671237feabe81b0bd35
Eisenwave <me@eisenwave.net>
2026-04-25 09:23:06 +0200

    Complete the implementation of wider_t

--- tests/beman/big_int/big_int.test.cpp
+++ tests/beman/big_int/big_int.test.cpp
@@ -7,6 +7,32 @@
 
 namespace beman::big_int {
 
+static_assert(detail::width_v<detail::wider_t<std::int8_t>> == 16);
+static_assert(detail::width_v<detail::wider_t<std::int16_t>> == 32);
+static_assert(detail::width_v<detail::wider_t<std::int32_t>> == 64);
+
+static_assert(detail::width_v<detail::wider_t<std::uint8_t>> == 16);
+static_assert(detail::width_v<detail::wider_t<std::uint16_t>> == 32);
+static_assert(detail::width_v<detail::wider_t<std::uint32_t>> == 64);
+
+#ifdef BEMAN_BIG_INT_HAS_WIDE_INT
+// The relationship between int_multiprecision_t and int_wide_t should be exact,
+// not just based on width.
+static_assert(std::is_same_v<detail::wider_t<detail::int_multiprecision_t>, detail::int_wide_t>);
+static_assert(std::is_same_v<detail::wider_t<uint_multiprecision_t>, detail::uint_wide_t>);
+#endif
+
+#ifdef BEMAN_BIG_INT_HAS_INT128
+static_assert(std::is_same_v<detail::wider_t<std::int64_t>, detail::int128_t>);
+static_assert(std::is_same_v<detail::wider_t<std::uint64_t>, detail::uint128_t>);
+#endif
+
+#ifdef BEMAN_BIG_INT_HAS_BITINT
+// wider_t should return a _BitInt when given a _BitInt.
+static_assert(std::is_same_v<detail::wider_t<_BitInt(32)>, _BitInt(64)>);
+static_assert(std::is_same_v<detail::wider_t<unsigned _BitInt(32)>, unsigned _BitInt(64)>);
+#endif
+
 template class basic_big_int<big_int::inplace_bits, big_int::allocator_type>;
 
 static_assert(basic_big_int<128, big_int::allocator_type>::inplace_bits == 128);
