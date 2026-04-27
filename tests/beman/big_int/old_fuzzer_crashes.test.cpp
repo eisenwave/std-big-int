b0332f7049998ed53a6c770141fd159fdffa4023
Matt Borland <matt@mattborland.com>
2026-04-27 18:08:07 -0400

    Add fuzzing of div and fix the position of remainder calculation (#160)

---
+++ tests/beman/big_int/old_fuzzer_crashes.test.cpp
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-License-Identifier: BSL-1.0
+
+#include "boost_mp_testing.hpp"
+#include "testing.hpp"
+#include <functional>
+
+using ::beman::big_int::boost_mp_testing::check_cpp_int_equal;
+
+TEST(OldFuzzerCrashes, Div1) {
+    EXPECT_TRUE(check_cpp_int_equal(std::divides<>{},
+                                    "010000000000000000141400000000000000000000000000000000ffffffff0000",
+                                    "00000000000000000065000000000000000000000000000000000000000000141402"));
+}
+
+TEST(OldFuzzerCrashes, Div2) {
+    EXPECT_TRUE(check_cpp_int_equal(
+        std::divides<>{}, "0100000000002400000000000000000000", "0000000000e7000000010000003100162400"));
+}
