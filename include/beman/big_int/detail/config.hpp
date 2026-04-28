4f0bc6d215c421a7d603eec27e2eb07416066cef
ckormanyos <ckormanyos@yahoo.com>
2026-04-28 21:41:08 +0200

    Remove header and fix dangling pragma push

--- include/beman/big_int/detail/config.hpp
+++ include/beman/big_int/detail/config.hpp
@@ -110,8 +110,6 @@
     // This case is for Clang when it provides _BitInt as an extension.
     #define BEMAN_BIG_INT_BITINT_MAXWIDTH __BITINT_MAXWIDTH__
     #define BEMAN_BIG_INT_HAS_BITINT 1
-    BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
-    BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wbit-int-extension")
 #else
     // Prevent warnings for use of undefined macros.
     #define BEMAN_BIG_INT_BITINT_MAXWIDTH 0
@@ -131,7 +129,33 @@
 
 // 128-bit integer support =====================================================
 
-#include <beman/big_int/detail/config128.hpp>
+#ifdef BEMAN_BIG_INT_MSVC
+    #include <__msvc_int128.hpp>
+#endif // BEMAN_BIG_INT_MSVC
+
+namespace beman::big_int::detail {
+
+#if BEMAN_BIG_INT_BITINT_MAXWIDTH >= 128
+    #define BEMAN_BIG_INT_HAS_INT128 1
+    #define BEMAN_BIG_INT_HAS_INT128_FUNDAMENTAL 1
+BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
+BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wbit-int-extension")
+using int128_t  = _BitInt(128);
+using uint128_t = unsigned _BitInt(128);
+BEMAN_BIG_INT_DIAGNOSTIC_POP()
+#elif defined(__SIZEOF_INT128__)
+    #define BEMAN_BIG_INT_HAS_INT128 1
+    #define BEMAN_BIG_INT_HAS_INT128_FUNDAMENTAL 1
+using int128_t  = __int128;
+using uint128_t = unsigned __int128;
+#elif defined(BEMAN_BIG_INT_MSVC)
+    #define BEMAN_BIG_INT_HAS_INT128 1
+    #define BEMAN_BIG_INT_HAS_INT128_CLASS 1
+using int128_t  = std::_Signed128;
+using uint128_t = std::_Unsigned128;
+#endif
+
+} // namespace beman::big_int::detail
 
 // Limb type selection =========================================================
 
