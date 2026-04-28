2d9612bb74d2617474f54303ad46faca803edf6d
ckormanyos <ckormanyos@yahoo.com>
2026-04-28 20:34:46 +0200

    Try isolate bit int detection

--- include/beman/big_int/detail/config.hpp
+++ include/beman/big_int/detail/config.hpp
@@ -99,23 +99,7 @@
 
 // _BitInt detection ===========================================================
 
-#include <climits> // for BITINT_MAXWIDTH
-
-#ifdef BITINT_MAXWIDTH
-    // Once _BitInt is a standard feature and available on all compilers,
-    // this case should be selected for all compilers.
-    #define BEMAN_BIG_INT_BITINT_MAXWIDTH BITINT_MAXWIDTH
-    #define BEMAN_BIG_INT_HAS_BITINT 1
-#elif defined(__BITINT_MAXWIDTH__)
-    // This case is for Clang when it provides _BitInt as an extension.
-    #define BEMAN_BIG_INT_BITINT_MAXWIDTH __BITINT_MAXWIDTH__
-    #define BEMAN_BIG_INT_HAS_BITINT 1
-    BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
-    BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wbit-int-extension")
-#else
-    // Prevent warnings for use of undefined macros.
-    #define BEMAN_BIG_INT_BITINT_MAXWIDTH 0
-#endif // BITINT_MAXWIDTH
+#include <beman/big_int/detail/config_bit_int_detect.hpp> // for BITINT_MAXWIDTH
 
 // 32-bit/64-bit ===============================================================
 
---
+++ include/beman/big_int/detail/config_bit_int_detect.hpp
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef BEMAN_BIG_INT_CONFIG_BIT_INT_DETECT_HPP
+#define BEMAN_BIG_INT_CONFIG_BIT_INT_DETECT_HPP
+
+// _BitInt detection ===========================================================
+
+#include <climits> // for BITINT_MAXWIDTH
+
+#ifdef BITINT_MAXWIDTH
+    // Once _BitInt is a standard feature and available on all compilers,
+    // this case should be selected for all compilers.
+    #define BEMAN_BIG_INT_BITINT_MAXWIDTH BITINT_MAXWIDTH
+    #define BEMAN_BIG_INT_HAS_BITINT 1
+#elif defined(__BITINT_MAXWIDTH__)
+    // This case is for Clang when it provides _BitInt as an extension.
+    #define BEMAN_BIG_INT_BITINT_MAXWIDTH __BITINT_MAXWIDTH__
+    #define BEMAN_BIG_INT_HAS_BITINT 1
+    BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
+    BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wbit-int-extension")
+#else
+    // Prevent warnings for use of undefined macros.
+    #define BEMAN_BIG_INT_BITINT_MAXWIDTH 0
+#endif // BITINT_MAXWIDTH
+
+#endif // BEMAN_BIG_INT_CONFIG_BIT_INT_DETECT_HPP
