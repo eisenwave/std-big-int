128d3d5b9b577474775f87d71a6d7dc7735dc0c8
ckormanyos <ckormanyos@yahoo.com>
2026-04-29 21:46:05 +0200

    Handle final lint (hopefully)

--- tests/beman/big_int/conversion.test.cpp
+++ tests/beman/big_int/conversion.test.cpp
@@ -249,8 +249,7 @@ TEST(Conversion, ToLongDoubleThreeLimbTieWithStickyBit) {
     BEMAN_BIG_INT_DIAGNOSTIC_PUSH()
     BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_GCC("-Wfloat-equal")
     BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wfloat-equal")
-    constexpr auto expected_int =
-        (static_cast<ubitint_n_t<192>>(1) << 191) | (static_cast<ubitint_n_t<128>>(1));
+    constexpr auto expected_int = (static_cast<ubitint_n_t<192>>(1) << 191) | (static_cast<ubitint_n_t<128>>(1));
     static_assert(static_cast<long double>(expected_int) == expected_int);
     BEMAN_BIG_INT_DIAGNOSTIC_POP()
 #endif
