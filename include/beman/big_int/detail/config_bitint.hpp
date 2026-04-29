9d3912bf1da4b1126d2dffd5117105cd2438e862
Matt Borland <matt@mattborland.com>
2026-04-29 16:18:49 -0400

    Mark as extensions instead of as system header

--- include/beman/big_int/detail/config_bitint.hpp
+++ include/beman/big_int/detail/config_bitint.hpp
@@ -4,21 +4,19 @@
 #ifndef BEMAN_BIG_INT_CONFIG_BITINT_HPP
 #define BEMAN_BIG_INT_CONFIG_BITINT_HPP
 
-#if defined(BEMAN_BIG_INT_GCC) || defined(BEMAN_BIG_INT_CLANG)
-    #pragma GCC system_header
-#endif
-
 // _BitInt support =====================================================
 
-using bitint32_t   = _BitInt(32);
-using ubitint32_t  = unsigned _BitInt(32);
-using bitint64_t   = _BitInt(64);
-using ubitint64_t  = unsigned _BitInt(64);
-using bitint128_t  = _BitInt(128);
-using ubitint128_t = unsigned _BitInt(128);
-template <const unsigned N>
+__extension__ using bitint32_t   = _BitInt(32);
+__extension__ using ubitint32_t  = unsigned _BitInt(32);
+__extension__ using bitint64_t   = _BitInt(64);
+__extension__ using ubitint64_t  = unsigned _BitInt(64);
+__extension__ using bitint128_t  = _BitInt(128);
+__extension__ using ubitint128_t = unsigned _BitInt(128);
+
+__extension__ template <const unsigned N>
 using bitint_n_t = _BitInt(N);
-template <const unsigned N>
+
+__extension__ template <const unsigned N>
 using ubitint_n_t = unsigned _BitInt(N);
 
 #endif // BEMAN_BIG_INT_CONFIG_BITINT_HPP
