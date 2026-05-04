7eff99b0b45a25ee616be300c1738a7349581da2
ckormanyos <ckormanyos@yahoo.com>
2026-05-04 11:26:41 +0200

    Attempt to handle firsrt review

--- examples/fibonacci.cpp
+++ examples/fibonacci.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 template <typename BigIntType>
 auto fibonacci(unsigned int n) -> BigIntType {
@@ -27,32 +28,21 @@ auto fibonacci(unsigned int n) -> BigIntType {
 }
 
 auto main() -> int {
-    using big_int_type = beman::big_int::big_int;
+    using beman::big_int::big_int;
 
     // Compute the 1,000th Fibonacci number.
-    const big_int_type big_int_fibonacci{fibonacci<big_int_type>(1000)};
+    const big_int fib_1000{fibonacci<big_int>(1000)};
 
-    constexpr std::size_t fib_buffer_size{256U};
+    const std::string str_fib_1000{to_string(fib_1000)};
 
-    char* p_str_fib = new char[fib_buffer_size];
+    const std::string str_ctrl{"4346655768693745643568852767504062580256466051737178040248172908"
+                               "9536555417949051890403879840079255169295922593080322634775209689"
+                               "6232398733224711616429964409065331879382989696499285160037044761"
+                               "37795166849228875"};
 
-    std::fill(p_str_fib, p_str_fib + fib_buffer_size, '\0');
+    const bool result_is_ok{str_fib_1000 == str_ctrl};
 
-    // TODO(ckormanyos): In the future, all of this printing can be accomplished with
-    //                   I/O-streaming and (potential) support for std::format-like
-    //                   mechanisms.
-
-    to_chars(p_str_fib, p_str_fib + fib_buffer_size, big_int_fibonacci);
-
-    const std::string str_ctrl{
-        "4346655768693745643568852767504062580256466051737178040248172908"
-        "9536555417949051890403879840079255169295922593080322634775209689"
-        "6232398733224711616429964409065331879382989696499285160037044761"
-        "37795166849228875"};
-
-    const bool result_is_ok{std::string{p_str_fib} == str_ctrl};
-
-    std::cout << "big_int_fibonacci:\n" << p_str_fib << "\n\nresult_is_ok:\n" << std::boolalpha << result_is_ok << std::endl;
-
-    delete [] p_str_fib;
+    std::cout << "fib_1000:\n"
+              << str_fib_1000 << "\n\nresult_is_ok:\n"
+              << std::boolalpha << result_is_ok << std::endl;
 }
