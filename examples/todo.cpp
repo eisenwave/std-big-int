935bd75705a61734d7df85d1cd9c5e2a3c1a8159
Matt Borland <matt@mattborland.com>
2026-06-16 11:16:31 -0400

    Remove TODO artifacts from exemplar repo

--- examples/todo.cpp
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include <beman/big_int/todo.hpp>
-
-int main() {
-    // TODO
-}
--- include/beman/big_int/todo.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-License-Identifier: BSL-1.0
-
-#ifndef BEMAN_BIG_INT_TODO_HPP
-#define BEMAN_BIG_INT_TODO_HPP
-
-namespace beman::big_int {} // namespace beman::big_int
-
-#endif // BEMAN_BIG_INT_TODO_HPP
