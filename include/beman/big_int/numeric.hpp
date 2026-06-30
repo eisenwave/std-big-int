f00ef2b6054de3429f2e1444d0f592f66c5d9476
Matt Borland <matt@mattborland.com>
2026-06-30 16:44:51 -0400

    Add sat cast impl

--- include/beman/big_int/numeric.hpp
+++ include/beman/big_int/numeric.hpp
@@ -21,6 +21,40 @@ constexpr std::remove_cvref_t<T> abs(T&& x) {
     return result;
 }
 
+// [numeric.sat.cast]
+// Casts `x` to `R`, clamping to the range of `R`. If the integer value of `x`
+// is representable as `R`, that value is returned; otherwise the largest or
+// smallest representable value of `R`, whichever is closer to `x`.
+template <class R, std::size_t b, class A>
+    requires detail::signed_or_unsigned<R>
+constexpr R saturating_cast(const basic_big_int<b, A>& x) noexcept {
+    using U                     = detail::make_unsigned_t<R>;
+
+    constexpr std::size_t width = detail::width_v<R>;
+    if constexpr (detail::unsigned_integer<R>) {
+        constexpr R hi = static_cast<R>(~U{0});
+
+        if (x < R{0}) {
+            return R{0};
+        }
+        if (x > hi) {
+            return hi;
+        }
+    } else {
+        constexpr R lo = static_cast<R>(U{1} << (width - 1));
+        constexpr R hi = static_cast<R>((U{1} << (width - 1)) - U{1});
+
+        if (x < lo) {
+            return lo;
+        }
+        if (x > hi) {
+            return hi;
+        }
+    }
+
+    return static_cast<R>(x);
+}
+
 } // namespace beman::big_int
 
 #endif // BEMAN_BIG_INT_NUMERIC_HPP
