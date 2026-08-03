3cb7d6bcb2689da5464422b6e5eee40110b0e5fa
ckormanyos <ckormanyos@yahoo.com>
2026-08-03 16:15:51 +0200

    Get x86_64 ASM running try CI

--- include/beman/big_int/detail/multiply_long_runtime.hpp
+++ include/beman/big_int/detail/multiply_long_runtime.hpp
@@ -17,62 +17,53 @@ BEMAN_BIG_INT_DIAGNOSTIC_IGNORED_CLANG("-Wcomment")
 //                   Only one macro should be activated at any time.
 //                   Note that the upper macro spans two lines.
 
-// #define MULTIPLY_LONG_RUNTIME(PARAM_RESULT, PARAM_A, PARAM_B) \
-//     multiply_long_runtime((PARAM_RESULT).data(), (PARAM_A).data(), (PARAM_A).size(), (PARAM_B).data(),
-//     (PARAM_B).size())
-#define MULTIPLY_LONG_RUNTIME(PARAM_RESULT, PARAM_A, PARAM_B) multiply_long((PARAM_RESULT), (PARAM_A), (PARAM_B))
+#define MULTIPLY_LONG_RUNTIME(PARAM_RESULT, PARAM_A, PARAM_B) \
+    ::multiply_long_runtime(                                  \
+        (PARAM_RESULT).data(), (PARAM_A).data(), (PARAM_A).size(), (PARAM_B).data(), (PARAM_B).size())
+// #define MULTIPLY_LONG_RUNTIME(PARAM_RESULT, PARAM_A, PARAM_B) multiply_long((PARAM_RESULT), (PARAM_A), (PARAM_B))
 
 BEMAN_BIG_INT_DIAGNOSTIC_POP()
 
 #if defined(BEMAN_BIG_INT_ARCH_X86_64_GCC_OR_CLANG)
+
 extern "C" void multiply_long_runtime(beman::big_int::uint_multiprecision_t*       p_result,
                                       const beman::big_int::uint_multiprecision_t* p_a,
                                       const std::size_t                            len_a,
                                       const beman::big_int::uint_multiprecision_t* p_b,
                                       const std::size_t                            len_b) noexcept;
+
 #else
-extern "C" inline void multiply_long_runtime(beman::big_int::uint_multiprecision_t* /*p_result*/,
-                                             const beman::big_int::uint_multiprecision_t* /*p_a*/,
-                                             const std::size_t /*len_a*/,
-                                             const beman::big_int::uint_multiprecision_t* /*p_b*/,
-                                             const std::size_t /*len_b*/) noexcept {}
-#endif
 
-#if !defined(BEMAN_BIG_INT_ARCH_X86_64_GCC_OR_CLANG)
-
-namespace beman::big_int::detail {
-
-extern "C" inline void multiply_long_runtime(uint_multiprecision_t*       p_result,
-                                             const uint_multiprecision_t* p_a,
-                                             const std::size_t            len_a,
-                                             const uint_multiprecision_t* p_b,
-                                             const std::size_t            len_b) noexcept {
+extern "C" inline void multiply_long_runtime(beman::big_int::uint_multiprecision_t*       p_result,
+                                             const beman::big_int::uint_multiprecision_t* p_a,
+                                             const std::size_t                            len_a,
+                                             const beman::big_int::uint_multiprecision_t* p_b,
+                                             const std::size_t                            len_b) noexcept {
     {
-        uint_multiprecision_t carry = 0;
+        beman::big_int::uint_multiprecision_t carry = 0;
         for (std::size_t j = 0; j < len_b; ++j) {
-            const auto [lo, hi] = widening_mul(*p_a, *(p_b + j));
-            const auto [s, c]   = carrying_add(lo, carry);
+            const auto [lo, hi] = beman::big_int::detail::widening_mul(*p_a, *(p_b + j));
+            const auto [s, c]   = beman::big_int::detail::carrying_add(lo, carry);
             *(p_result + j)     = s;
-            carry               = hi + static_cast<uint_multiprecision_t>(c);
+            carry               = hi + static_cast<beman::big_int::uint_multiprecision_t>(c);
         }
         *(p_result + len_b) = carry;
     }
 
     // Subsequent rows: accumulate onto values written by previous rows.
     for (std::size_t i = 1; i < len_a; ++i) {
-        uint_multiprecision_t carry = 0;
+        beman::big_int::uint_multiprecision_t carry = 0;
         for (std::size_t j = 0; j < len_b; ++j) {
-            const auto [lo, hi]   = widening_mul(*(p_a + i), *(p_b + j));
-            const auto [s1, c1]   = carrying_add(lo, *(p_result + (i + j)));
-            const auto [s2, c2]   = carrying_add(s1, carry);
+            const auto [lo, hi]   = beman::big_int::detail::widening_mul(*(p_a + i), *(p_b + j));
+            const auto [s1, c1]   = beman::big_int::detail::carrying_add(lo, *(p_result + (i + j)));
+            const auto [s2, c2]   = beman::big_int::detail::carrying_add(s1, carry);
             *(p_result + (i + j)) = s2;
-            carry = hi + static_cast<uint_multiprecision_t>(c1) + static_cast<uint_multiprecision_t>(c2);
+            carry                 = hi + static_cast<beman::big_int::uint_multiprecision_t>(c1) +
+                                    static_cast<beman::big_int::uint_multiprecision_t>(c2);
         }
         *(p_result + (i + len_b)) = carry;
     }
 }
-
-} // namespace beman::big_int::detail
 
 #endif // !defined(BEMAN_BIG_INT_ARCH_X86_64_GCC_OR_CLANG)
 
